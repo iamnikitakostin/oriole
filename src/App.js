2cedc616425e7b129e22908d9c895bed0301f9bd
iamnikitakostin <141674557+iamnikitakostin@users.noreply.github.com>
2024-05-03 19:47:46 -0700

    build

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import About from './pages/About/About';
 import Gallery from './pages/Gallery/Gallery';
 import Menu from './pages/Menu/Menu';
 import ContactUs from './pages/ContactUs/ContactUs';
-
+import React from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const [loaded, setLoaded] = React.useState(false);
+  React.useEffect(() => {
+    console.log(loaded)
+    if (loaded) {
+      setTimeout(() => {
+        document.body.style.overflow = '';
+      }, 250);
+    } else {
+      document.body.style.overflow = 'hidden';
+    }
+
+  }, [loaded]);
   return (
     <div className="App">
-      <Landing />
+      {!loaded && (
+        <div className={`loading ${loaded ? 'fade-out' : ''}`}>
+        <ClipLoader
+          color="#f5a201"
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+      )}
+      <Landing setLoaded={setLoaded}/>
       <About />
       <Menu />
       <Gallery />
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -4,11 +4,11 @@ import "./Landing.css"
 import Nav from '../../components/Nav/Nav'
 import Hero from '../../components/Hero/Hero'
 
-function Landing() {
+function Landing({setLoaded}) {
   return (
     <div className='app__landing' id="home">
         <Hero/>
-        <Nav/>
+        <Nav setLoaded={setLoaded}/>
         <div className="app__landing-bg"></div>
     </div>
   )
